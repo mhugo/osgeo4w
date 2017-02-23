ba80bb6e22987aa34cd91314bdf5794d6ac76fe9
Hugo Mercier <hugo.mercier@oslandia.com>
2017-02-23 11:32:51 +0100

    Add tar creation

--- protobuf/package.cmd
+++ protobuf/package.cmd
@@ -1,15 +1,19 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-set PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files\PostgreSQL\9.6\bin
+set PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files\PostgreSQL\9.6\bin;C:\cygwin64\bin
+
+set P=protobuf
+set V=3.1.0
+set B=1
+set R=X:\X86_64\release\\%P%
+mkdir %R%
+
 wget --progress=bar:force https://github.com/google/protobuf/releases/download/v3.1.0/protobuf-cpp-3.1.0.zip
 unzip -q protobuf-cpp-3.1.0.zip
 cd protobuf-3.1.0\cmake
 mkdir build
 cd build
-rd /s /q c:\libs
-cmake -G "NMake Makefiles" -Dprotobuf_BUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=C:\libs -Dprotobuf_BUILD_TESTS=OFF ..
+rd /s /q c:\install
+cmake -G "NMake Makefiles" -Dprotobuf_BUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=C:\install -Dprotobuf_BUILD_TESTS=OFF ..
 nmake
 nmake install
-cd c:\libs
-xcopy /s bin x:\x86_64\release\protobuf\
-xcopy /s lib x:\x86_64\release\protobuf\
-xcopy /s include x:\x86_64\release\protobuf\
+tar -C c:\install -cjf %R%\%P%-%V%-%B%.tar.bz2 include lib bin

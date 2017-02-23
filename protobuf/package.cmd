630ae455608ef3310f2877d6836ef4f28cd5c0bb
Hugo Mercier <hugo.mercier@oslandia.com>
2017-02-23 14:37:33 +0100

    Use ssh to copy build outputs

--- protobuf/package.cmd
+++ protobuf/package.cmd
@@ -4,8 +4,10 @@ set PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files\PostgreSQL\9.6\bin;C
 set P=protobuf
 set V=3.1.0
 set B=1
-set R=X:\X86_64\release\\%P%
-mkdir %R%
+set H=ci@hekla.oslandia.net
+set D=/home/storage/osgeo4w/x86_64/release/%P%
+set R=%H%:%D
+ssh %H% "mkdir -p %D"
 
 wget --progress=bar:force https://github.com/google/protobuf/releases/download/v3.1.0/protobuf-cpp-3.1.0.zip
 unzip -q protobuf-cpp-3.1.0.zip
@@ -16,4 +18,6 @@ rd /s /q c:\install
 cmake -G "NMake Makefiles" -Dprotobuf_BUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=C:\install -Dprotobuf_BUILD_TESTS=OFF ..
 nmake
 nmake install
-tar -C c:\install -cjf %R%\%P%-%V%-%B%.tar.bz2 include lib bin
+tar -C c:\install -cjf %P%-%V%-%B%.tar.bz2 include lib bin
+
+scp %P%-%V%-%B%.tar.bz2 %R%

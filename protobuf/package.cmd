ecdb3d4bca33e1ec732096140e4ffca664af0418
Hugo Mercier <hugo.mercier@oslandia.com>
2017-02-24 09:21:08 +0100

    wip

--- protobuf/package.cmd
+++ protobuf/package.cmd
@@ -10,15 +10,15 @@ set B=1
 ..\inc\prepare_env.bat
 
 ::--------- Build script
-::wget --progress=bar:force https://github.com/google/protobuf/releases/download/v3.1.0/protobuf-cpp-3.1.0.zip
-::unzip -q protobuf-cpp-3.1.0.zip
+wget --progress=bar:force https://github.com/google/protobuf/releases/download/v3.1.0/protobuf-cpp-3.1.0.zip
+unzip -q protobuf-cpp-3.1.0.zip
 
 cd protobuf-3.1.0\cmake
-::mkdir build
+mkdir build
 cd build
 :: make sure the install dir is empty before installing
-::rd /s /q c:\install
-::cmake -G "NMake Makefiles" -Dprotobuf_BUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=C:\install -Dprotobuf_BUILD_TESTS=OFF ..
+rd /s /q c:\install
+cmake -G "NMake Makefiles" -Dprotobuf_BUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=C:\install -Dprotobuf_BUILD_TESTS=OFF ..
 nmake && nmake install
 if %ERRORLEVEL% NEQ 0 (
    exit /b 1

f50f075b172e172ff04a69465677a2002ad35477
Hugo Mercier <hugo.mercier@oslandia.com>
2017-02-28 08:29:13 +0100

    Fix boost dirs

--- boost/package.cmd
+++ boost/package.cmd
@@ -10,9 +10,9 @@ set B=1
 call ..\inc\prepare_env.bat
 
 wget --progress=bar:force https://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.bz2
-tar xjvf boost-1.63.0.tar.bz2
+tar xjvf boost_1_63_0.tar.bz2
 
-cd boost-1.63.0
+cd boost_1_63_0
 call bootstrap.bat --with-python=C:\osgeo4W64\apps\python36
 call b2.bat --with-python
 

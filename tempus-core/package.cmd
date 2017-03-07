ad7a82e9bc7b4ea22ef027b2999737af90f688eb
Hugo Mercier <hugo.mercier@oslandia.com>
2017-03-07 15:23:25 +0100

    Update tempus packageing

--- tempus-core/package.cmd
+++ tempus-core/package.cmd
@@ -2,7 +2,7 @@
 :: package name
 set P=tempus-core
 :: version
-set V=2.2.0
+set V=2.3.0
 :: package version
 set B=1
 
@@ -10,34 +10,16 @@ set B=1
 call ..\inc\prepare_env.bat
 
 c:\osgeo4w64\bin\osgeo4w-setup.exe -s http://hekla.oslandia.net/osgeo4w -k -q -P boost-devel
-wget --progress=bar:force https://gitlab.com/Oslandia/Tempus/repository/archive.tar.bz2?ref=testci -O tempus.tar.bz2
+wget --progress=bar:force https://gitlab.com/Oslandia/Tempus/repository/archive.tar.bz2?ref=master -O tempus.tar.bz2
 tar xjf tempus.tar.bz2
 cd Tempus-*
-mkdir build
-cd build
-cmake -G "NMake Makefiles" ^
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
-  -DCMAKE_INSTALL_PREFIX=c:\install ^
-  "-DPOSTGRESQL_PG_CONFIG=C:\Program Files\Postgresql\9.6\bin\pg_config.exe" ^
-  -DBOOST_LIBRARYDIR=C:\osgeo4w64\lib ^
-  -DBOOST_INCLUDEDIR=C:\osgeo4w64\include\boost-1_63 ^
-  -DBoost_COMPILER=-vc140 ^
-  -DLIBXML2_INCLUDE_DIR=C:\osgeo4w64\include ^
-  -DLIBXML2_LIBRARIES=C:\osgeo4w64\lib\libxml2.lib ^
-  -DFCGI_INCLUDE_DIR=C:\osgeo4w64\include ^
-  -DFCGI_LIBRARIES=C:\osgeo4w64\lib\libfcgi.lib ^
-  -DBUILD_WPS=ON ^
-  -DBUILD_QGIS_PLUGIN=OFF ^
-  -DBUILD_OSM2TEMPUS=OFF ^
-  -DBUILD_DOC=OFF ^
-  ..
-nmake && nmake install
+call ci\windows\build_gitlab.bat
 if %ERRORLEVEL% NEQ 0 (
    exit /b 1
 )
 
 :: binary archive
-tar -C c:\ --transform 's,install,apps/tempus,' -cvjf %PKG_BIN% install
+tar -C build --transform 's,install,apps/tempus,' -cvjf %PKG_BIN% install
 
 :: source archive
 tar -C %HERE% --transform 's,^,osgeo4w/,' -cvjf %PKG_SRC% package.cmd setup.hint

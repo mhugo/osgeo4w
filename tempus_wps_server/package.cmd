c71b3baeae1c54093e77abacde66006524bf7cae
Hugo Mercier <hugo.mercier@oslandia.com>
2017-03-08 16:51:23 +0100

    Add Tempus WPS server

---
+++ tempus_wps_server/package.cmd
@@ -0,0 +1,28 @@
+::--------- Package settings --------
+:: package name
+set P=tempus-wps-server
+:: version
+set V=1.0.0
+:: package version
+set B=1
+
+::--------- Prepare the environment
+call ..\inc\prepare_env.bat
+
+c:\osgeo4w64\bin\osgeo4w-setup.exe -s http://hekla.oslandia.net/osgeo4w -k -q -P tempus-core -P boost-devel -P fcgi -P libxml2
+wget --progress=bar:force https://gitlab.com/Oslandia/tempus_wps_server/repository/archive.tar.bz2?ref=master -O tempus_wps_server.tar.bz2
+tar xjf tempus_wps_server.tar.bz2
+cd tempus_wps_server-*
+call ci\windows\build_gitlab.bat
+if %ERRORLEVEL% NEQ 0 (
+   exit /b 1
+)
+
+:: binary archive
+tar --transform 's,install,apps/tempus,' -cvjf %PKG_BIN% install
+
+:: source archive
+tar -C %HERE% --transform 's,^,osgeo4w/,' -cvjf %PKG_SRC% package.cmd setup.hint
+
+::--------- Installation
+call %HERE%\..\inc\install_archives.bat

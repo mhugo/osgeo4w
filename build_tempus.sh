57f6801053f95f165b6da288490d70c5fb6e223b
Hugo Mercier <hugo.mercier@oslandia.com>
2017-03-08 17:49:27 +0100

    Add a main 'tempus' script

---
+++ build_tempus.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# build all packages by order of dependencies
+for pkg in "protobuf tempus-core tempus_wps_server pytempus osm2tempus tempus_loader"
+do
+    cmd /s /c "cd $pkg && package.cmd"
+    # rebuild setup.ini on hekla
+    scp genini gen_setup_ini.sh ci@hekla.oslandia.net:~/
+    ssh ci@hekla.oslandia.net ./gen_setup_ini.sh
+done

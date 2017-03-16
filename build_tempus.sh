de9148801b8152f2280357124e0bc1be321d67b3
Hugo Mercier <hugo.mercier@oslandia.com>
2017-03-16 10:43:13 +0100

    Stop on problem

--- build_tempus.sh
+++ build_tempus.sh
@@ -3,6 +3,10 @@
 for pkg in protobuf tempus-core tempus_wps_server pytempus osm2tempus tempus_loader
 do
     cmd /s /c "cd $pkg && package.cmd test"
+    if [ "$?" != "0" ]; then
+        echo "Problem in last build"
+        exit 1
+    fi
     # rebuild setup.ini on hekla
     scp genini gen_setup_ini.sh ci@hekla.oslandia.net:~/
     ssh ci@hekla.oslandia.net ./gen_setup_ini.sh test

608e90720021f632cf55bbcc4330ae552a69b608
Vincent Mora <vincent.mora@oslandia.com>
2017-04-14 15:56:59 +0200

    adds python3-shapely

---
+++ python3-shapely/package.cmd
@@ -0,0 +1,27 @@
+::--------- Package settings --------
+:: package name
+set P=hydra_package
+:: version
+set V=0.0.23
+:: package version
+set B=1
+
+set HERE=%CD%
+
+::--------- Prepare the environment
+set OSGEO4W_HOME=c:\osgeo4w64
+call ..\inc\prepare_env.bat %1
+call %OSGEO4W_HOME%\bin\o4w_env.bat
+set PYTHONHOME=%OSGEO4W_HOME%\apps\Python36
+
+chcp 65001
+set GEOS_LIBRARY_PATH=%OSGEO4W_HOME%
+python3 -m pip install shapely
+
+:: binary archive
+tar --transform 's,%OSGEO4W_HOME%,,' -cvjf %PKG_BIN% %PYTHONHOME%/site-packages/shapely
+
+::--------- Installation
+scp %PKG_BIN% %R%
+cd %HERE%
+scp setup.hint %R%

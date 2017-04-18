02388be8ad093edb945ffe75a90fb0c25a0495e0
Hugo Mercier <hugo.mercier@oslandia.com>
2017-04-18 10:30:07 +0200

    Fix olefile package

--- python3-olefile/package.cmd
+++ python3-olefile/package.cmd
@@ -9,15 +9,14 @@ set B=1
 set HERE=%CD%
 
 ::--------- Prepare the environment
-set OSGEO4W_HOME=c:\osgeo4w64
 call ..\inc\prepare_env.bat %1
-call %OSGEO4W_HOME%\bin\o4w_env.bat
-set PYTHONHOME=%OSGEO4W_HOME%\apps\Python36
+set OSGEO4W_ROOT=c:\osgeo4w64
+call %OSGEO4W_ROOT\bin\py3_env.bat
 
 python3 -m pip install olefile==0.44
 
 :: binary archive
-tar --transform 's,%OSGEO4W_HOME%,,' -cvjf %PKG_BIN% %PYTHONHOME%/site-packages/olefile
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python36/Lib/site-packages/olefile
 
 ::--------- Installation
 scp %PKG_BIN% %R%

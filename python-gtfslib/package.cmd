2a5bc37f86464bc80450f039b0f364db26aeab2b
Hugo Mercier <hugo.mercier@oslandia.com>
2017-04-18 13:57:00 +0200

    Add gtfslib

---
+++ python-gtfslib/package.cmd
@@ -0,0 +1,26 @@
+::--------- Package settings --------
+:: package name
+set P=python-gtfslib
+:: version
+set V=1.0.0
+:: package version
+set B=1
+
+set HERE=%CD%
+
+::--------- Prepare the environment
+call ..\inc\prepare_env.bat %1
+set OSGEO4W_ROOT=c:\osgeo4w64
+set PATH=%OSGEO4W_ROOT%\bin;%PATH%
+
+:: python2 package
+call %OSGEO4W_ROOT%\etc\ini\python-core.bat
+
+pip install gtfslib
+
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/gtfslib
+
+::--------- Installation
+scp %PKG_BIN% %R%
+cd %HERE%
+scp setup.hint %R%

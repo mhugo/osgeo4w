9776e943adc94af89ea187feb46f05ad254ad915
Hugo Mercier <hugo.mercier@oslandia.com>
2017-04-18 10:54:21 +0200

    Fix shapely packaging

--- python3-shapely/package.cmd
+++ python3-shapely/package.cmd
@@ -9,20 +9,20 @@ set B=1
 set HERE=%CD%
 
 ::--------- Prepare the environment
-set OSGEO4W_HOME=c:\osgeo4w64
 call ..\inc\prepare_env.bat %1
-call %OSGEO4W_HOME%\bin\o4w_env.bat
-set PYTHONHOME=%OSGEO4W_HOME%\apps\Python36
+set OSGEO4W_ROOT=c:\osgeo4w64
+call %OSGEO4W_ROOT%\bin\py3_env.bat
+set PATH=%OSGEO4W_ROOT%\bin;%PATH%
 
 chcp 65001
-set GEOS_LIBRARY_PATH=%OSGEO4W_HOME%\bin\geos_c.dll
+set GEOS_LIBRARY_PATH=%OSGEO4W_ROOT%\bin\geos_c.dll
 python3 -m pip install shapely==1.5.17
 
 :: need to replace geo.dll by geos_c.dll in package
-python3 -c 'print open("%PYTHONHOME%/site-packages/shapely/geos.py").read().replace("geos.dll","geos_c.dll")'
+sed -i s/geos.dll/geos_c.dll/g %PYTHONHOME%/Lib/site-packages/shapely/geos.py
 
 :: binary archive
-tar --transform 's,%OSGEO4W_HOME%,,' -cvjf %PKG_BIN% %PYTHONHOME%/site-packages/shapely
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python36/Lib/site-packages/shapely
 
 ::--------- Installation
 scp %PKG_BIN% %R%

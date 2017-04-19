1a713e1baebc0e1e80bc4ee69ac80661d5f2b16d
Vincent Mora <vincent.mora@oslandia.com>
2017-04-19 18:04:58 +0200

    try exit code on postinst

--- hydra_client/postinstall.bat
+++ hydra_client/postinstall.bat
@@ -2,3 +2,4 @@ call bin\o4w_env.bat > var/log/hydra_client.log
 python -m pip install --upgrade setuptools < var/log/hydra_client.log
 python -m pip install matplotlib < var/log/hydra_client.log
 python -m pip install PyOpenGL < var/log/hydra_client.log
+exit 0

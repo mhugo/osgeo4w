b49eca7e61d34bd5461f3dae607a6ebe98fa5136
Vincent Mora <vincent.mora@oslandia.com>
2017-04-19 15:37:49 +0200

    new package for hydra

---
+++ hydra_client/package.cmd
@@ -0,0 +1,21 @@
+::--------- Package settings --------
+:: package name
+set P=hydra_client
+:: version
+set V=0.0.23
+:: package version
+set B=1
+
+set HERE=%CD%
+
+::--------- Prepare the environment
+call ..\inc\prepare_env.bat %1
+
+
+:: binary archive
+tar --transform 's,postinstall.bat,etc/postinstall/hydra_client.bat,' -cvjf %PKG_BIN% postinstall.bat
+
+::--------- Installation
+scp %PKG_BIN% %R%
+cd %HERE%
+scp setup.hint %R%

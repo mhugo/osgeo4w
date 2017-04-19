4462cfcbb9b7123a43297e3cc4bd96daacf81f73
Vincent Mora <vincent.mora@oslandia.com>
2017-04-19 11:56:29 +0200

    fix rtree package

--- python-rtree/package.cmd
+++ python-rtree/package.cmd
@@ -18,6 +18,9 @@ call %OSGEO4W_ROOT%\etc\ini\python-core.bat
 
 pip install rtree==%V%
 
+sed -i s/spatialindex_c.dll/spatialindex_c-64.dll/g %PYTHONHOME%/Lib/site-packages/rtree/core.py
+
+
 tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/rtree
 
 ::--------- Installation

b09e35627840250040a08bc25fa51232613213cb
Vincent Mora <vincent.mora@oslandia.com>
2017-04-20 17:31:08 +0200

    Revert "Revert "fix missing files in package""
    This reverts commit c5a6f6ddad638ec100e92cc09c1a00dfbb08c12d.

--- python3-gdal/package.cmd
+++ python3-gdal/package.cmd
@@ -4,13 +4,16 @@ set P=python3-gdal
 :: version
 set V=2.1.3
 :: package version
-set B=2
+set B=3
 
 set HERE=%CD%
 
 ::--------- Prepare the environment
 call ..\inc\prepare_env.bat %1
 set OSGEO4W_ROOT=c:\osgeo4w64
+
+%OSGEO4W_ROOT%\bin\osgeo4w-setup.exe -s %OSGEO4W_REPO% -k -q -P gdal
+
 call %OSGEO4W_ROOT%\bin\py3_env.bat
 set PATH=%OSGEO4W_ROOT%\bin;%PATH%
 
@@ -19,7 +22,7 @@ python3 -m pip install numpy
 python3 -m pip install  --global-option=build_ext --global-option="-Ic:\OSGeo4W64\include" --global-option="-Lc:\OSGeo4W64\lib" gdal==2.1.3
 
 :: binary archive
-tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python36/Lib/site-packages/osgeo
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python36/Lib/site-packages/osgeo apps/Python36/Lib/site-packages/gdal.py apps/Python36/Lib/site-packages/ogr.py apps/Python36/Lib/site-packages/osr.py apps/Python36/Lib/site-packages/gdalconst.py apps/Python36/Lib/site-packages/gdalnumeric.py
 
 ::--------- Installation
 scp %PKG_BIN% %R%

87a1cfade0033f4bcaf58d0d5d0d3d8f68bb4d24
Vincent Mora <vincent.mora@oslandia.com>
2017-04-20 15:33:45 +0200

    fix gdal package

--- python3-gdal/package.cmd
+++ python3-gdal/package.cmd
@@ -14,7 +14,7 @@ set OSGEO4W_ROOT=c:\osgeo4w64
 call %OSGEO4W_ROOT%\bin\py3_env.bat
 set PATH=%OSGEO4W_ROOT%\bin;%PATH%
 
-python3 -m pip install  --global-option=build_ext --global-option="-Ic:\OSGeo4W64\include" --global-option="-Lc:\OSGeo4W64\lib" gdal==2.1.33
+python3 -m pip install  --global-option=build_ext --global-option="-Ic:\OSGeo4W64\include" --global-option="-Lc:\OSGeo4W64\lib" gdal==2.1.3
 
 :: binary archive
 tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python36/Lib/site-packages/osgeo

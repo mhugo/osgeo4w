b635dfaf79334a5bafcc5580ccda945bab51c97b
Vincent Mora <vincent.mora@oslandia.com>
2017-04-20 15:21:58 +0200

    fix package

--- python-pglite/package.cmd
+++ python-pglite/package.cmd
@@ -14,16 +14,24 @@ set OSGEO4W_ROOT=c:\osgeo4w64
 set PATH=%OSGEO4W_ROOT%\bin;%PATH%
 
 :: python2 package
-call %OSGEO4W_ROOT%\etc\ini\python-core.bat
+call %OSGEO4W_ROOT%\etc\ini\python-core.bat || goto :error
 
-wget https://github.com/Oslandia/pglite/archive/master.zip
-unzip master.zip
-cd master
-python setup.py install
+wget https://github.com/Oslandia/pglite/archive/master.zip || goto :error
+unzip master.zip || goto :error
+cd pglite-master || goto :error
+python setup.py install || goto :error
 
-tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/pglite
+cd %HERE%
+
+tar -C %OSGEO4W_ROOT% --transform 's,pglite-1.0.2-py2.7.egg/,,' -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/pglite-1.0.2-py2.7.egg/pglite || goto :error
 
 ::--------- Installation
-scp %PKG_BIN% %R%
-cd %HERE%
-scp setup.hint %R%
+scp %PKG_BIN% %R% || goto :error
+scp setup.hint %R% || goto :error
+call ..\inc\install_archives.bat || goto :error
+goto :EOF
+
+
+:error
+echo Build failed
+exit /b 1

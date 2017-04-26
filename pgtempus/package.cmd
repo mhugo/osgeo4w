20251cafdaf69ded3fdd8a9e9bbc7b20b8acea7b
Hugo Mercier <hugo.mercier@oslandia.com>
2017-04-26 17:29:18 +0200

    Add pgtempus

---
+++ pgtempus/package.cmd
@@ -0,0 +1,43 @@
+::--------- Package settings --------
+:: package name
+set P=pgtempus
+:: version
+set V=1.0.0
+:: package version
+set B=1
+
+set HERE=%CD%
+
+::--------- Prepare the environment
+call ..\inc\prepare_env.bat %1
+
+:: python3 env
+call C:\osgeo4W64\bin\py3_env.bat || goto :error
+
+wget https://gitlab.com/Oslandia/pgtempus/repository/archive.tar.bz2?ref=master || goto :error
+tar xjvf pgtempus.tar.bz2 || goto :error
+cd pgtempus-* || goto :error
+
+rd /s /q c:\install
+mkdir c:\install\share\extension
+mkdir c:\install\apps\Python36\Lib\site-packages\pgtempus
+mkdir c:\install\etc\postinstall
+
+copy pgtempus\*.* c:\install\apps\Python36\Lib\site-packages\pgtempus || goto :error
+copy pgext\pgtempus-*.* c:\install\share\extension || goto :error
+
+cd ..
+
+copy set_tempus_plugins_dir.bat c:\install\etc\postinstall
+
+tar -C c:\install -cvjf %PKG_BIN% apps share etc || goto :error
+
+::--------- Installation
+scp %PKG_BIN% %R% || goto :error
+scp setup.hint %R% || goto :error
+goto :EOF
+
+
+:error
+echo Build failed
+exit /b 1
---
+++ pgtempus/set_tempus_plugins_dir.bat
@@ -0,0 +1,1 @@
+%OSGEO4W_ROOT%\bin\textreplace -std -t %OSGEO4W_ROOT%\apps\Python36\Lib\site-packages\pgtempus\__init__.py

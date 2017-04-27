678a44c703535ba83e2880629e203edaf67ed5ea
Hugo Mercier <hugo.mercier@oslandia.com>
2017-04-27 16:45:00 +0200

    tempus installer 2.4.0

--- make_setup_exe/make_tempus_installer.sh
+++ make_setup_exe/make_tempus_installer.sh
@@ -1,1 +1,1 @@
-./make_setup_exe.sh -n tempus -v 2.3.0-rc1 -p tempus-core -p python3-pytempus -p osm2tempus -p python-tempus-loader -p tempus-wps-server
+./make_setup_exe.sh -n tempus -v 2.4.0-rc1 -p tempus-core -p python3-pytempus -p osm2tempus -p python-tempus-loader -p tempus-wps-server -p python-pglite -p python3-pglite

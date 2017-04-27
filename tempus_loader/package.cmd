3bbc56d144c6a760cc267fd6e6ae8adfd26206a3
Hugo Mercier <hugo.mercier@oslandia.com>
2017-04-27 10:52:59 +0200

    Add error handling to tempus_loader

--- tempus_loader/package.cmd
+++ tempus_loader/package.cmd
@@ -13,19 +13,22 @@ set OSGEO4W_ROOT=C:\osgeo4w64
 call c:\osgeo4w64\etc\ini\python-core.bat
 set PATH=%PATH%;c:\osgeo4w64\bin;c:\cygwin64\bin
 
-wget --progress=bar:force https://gitlab.com/Oslandia/tempus_loader/repository/archive.tar.bz2?ref=v1.0.0 -O tempus_loader.tar.bz2
+wget --progress=bar:force https://gitlab.com/Oslandia/tempus_loader/repository/archive.tar.bz2?ref=v1.0.0 -O tempus_loader.tar.bz2 || goto :error
 tar xjf tempus_loader.tar.bz2
 cd tempus_loader-*
-python setup.py install
-if %ERRORLEVEL% NEQ 0 (
-   exit /b 1
-)
+python setup.py install || goto :error
 
 :: binary archive
-tar -C c:\OSGeo4W64 -cjvf %PKG_BIN% apps/python27/lib/site-packages/tempusloader-%V%-py2.7.egg
+tar -C c:\OSGeo4W64 -cjvf %PKG_BIN% apps/python27/lib/site-packages/tempusloader-%V%-py2.7.egg || goto :error
 
 :: source archive
-tar -C %HERE% --transform 's,^,osgeo4w/,' -cvjf %PKG_SRC% package.cmd setup.hint
+tar -C %HERE% --transform 's,^,osgeo4w/,' -cvjf %PKG_SRC% package.cmd setup.hint || goto :error
 
 ::--------- Installation
-call %HERE%\..\inc\install_archives.bat
+call %HERE%\..\inc\install_archives.bat || goto :error
+
+goto :EOF
+
+:error
+echo Build failed
+exit /b 1

28ddb02a96c4ddeea1a82000ba8d8c63e4e6175a
Hugo Mercier <hugo.mercier@oslandia.com>
2017-05-19 14:24:59 +0200

    gtfslib patch

--- python-gtfslib/package.cmd
+++ python-gtfslib/package.cmd
@@ -2,9 +2,9 @@
 :: package name
 set P=python-gtfslib
 :: version
-set V=1.0.0
+set V=1.0.1
 :: package version
-set B=3
+set B=1
 
 set HERE=%CD%
 
@@ -17,6 +17,10 @@ set PATH=%OSGEO4W_ROOT%\bin;%PATH%
 call %OSGEO4W_ROOT%\etc\ini\python-core.bat
 
 pip install gtfslib
+
+cd %OSGEO4W_ROOT%\apps\python27\lib\site-packages
+patch -p1 < %HERE%\do_not_prefetch.patch
+cd %HERE%
 
 tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/gtfslib apps/Python27/Lib/site-packages/gtfslib-%V%-py2.7.egg-info
 

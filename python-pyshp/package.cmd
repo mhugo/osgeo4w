66382016125cd03829b1989e0710289ea4ad83ba
Hugo Mercier <hugo.mercier@oslandia.com>
2017-05-19 16:43:31 +0200

    Fix pyshp

--- python-pyshp/package.cmd
+++ python-pyshp/package.cmd
@@ -16,11 +16,18 @@ set PATH=%OSGEO4W_ROOT%\bin;%PATH%
 :: python2 package
 call %OSGEO4W_ROOT%\etc\ini\python-core.bat
 
-pip install pyshp==1.2.10
+pip install pyshp==1.2.10 || goto :error
 
-tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/shapefile.py apps/Python27/Lib/site-packages/pyshp-1.2.10-py2.7.egg-info
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/shapefile.py apps/Python27/Lib/site-packages/pyshp-1.2.10.dist-info || goto :error
 
 ::--------- Installation
-scp %PKG_BIN% %R%
+scp %PKG_BIN% %R% || goto :error
 cd %HERE%
-scp setup.hint %R%
+scp setup.hint %R% || goto :error
+
+goto :EOF
+
+:error
+echo Build failed
+exit /b 1
+

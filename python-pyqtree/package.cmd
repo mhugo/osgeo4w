c66a3d8ea307d32dcc216a326a059011ecae2e90
Hugo Mercier <hugo.mercier@oslandia.com>
2017-05-19 16:42:40 +0200

    Fix pyqtree

--- python-pyqtree/package.cmd
+++ python-pyqtree/package.cmd
@@ -4,7 +4,7 @@ set P=python-pyqtree
 :: version
 set V=0.24
 :: package version
-set B=2
+set B=3
 
 set HERE=%CD%
 
@@ -14,13 +14,19 @@ set OSGEO4W_ROOT=c:\osgeo4w64
 set PATH=%OSGEO4W_ROOT%\bin;%PATH%
 
 :: python2 package
-call %OSGEO4W_ROOT%\etc\ini\python-core.bat
+call %OSGEO4W_ROOT%\etc\ini\python-core.bat || goto :error
 
-pip install Pyqtree==0.24
+pip install Pyqtree==0.24 || goto :error
 
-tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/pyqtree.py apps/Python27/Lib/site-packages/Pyqtree-0.24-py2.7.egg-info
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/pyqtree.py apps/Python27/Lib/site-packages/Pyqtree-0.24.dist-info || goto :error
 
 ::--------- Installation
-scp %PKG_BIN% %R%
+scp %PKG_BIN% %R% || goto :error
 cd %HERE%
-scp setup.hint %R%
+scp setup.hint %R% || goto :error
+goto :EOF
+
+:error
+echo Build failed
+exit /b 1
+

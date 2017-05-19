5f3a9f470e39d9d8313217d1b765090752308bfa
Hugo Mercier <hugo.mercier@oslandia.com>
2017-05-19 14:50:01 +0200

    Fix gtfslib patch

--- python-gtfslib/package.cmd
+++ python-gtfslib/package.cmd
@@ -11,18 +11,19 @@ set HERE=%CD%
 ::--------- Prepare the environment
 call ..\inc\prepare_env.bat %1
 set OSGEO4W_ROOT=c:\osgeo4w64
-set PATH=%OSGEO4W_ROOT%\bin;%PATH%
+set PATH=c:\cygwin64\bin;%OSGEO4W_ROOT%\bin;%PATH%
 
 :: python2 package
 call %OSGEO4W_ROOT%\etc\ini\python-core.bat
 
 pip install gtfslib
 
-cd %OSGEO4W_ROOT%\apps\python27\lib\site-packages
-patch -p1 < %HERE%\do_not_prefetch.patch
-cd %HERE%
+mkdir gtfslib
+copy %OSGEO4W_ROOT%\apps\python27\lib\site-packages\gtfslib\converter.py gtfslib
+patch -p1 < do_not_prefetch.patch 
+copy /Y gtfslib\converter.py %OSGEO4W_ROOT%\apps\python27\lib\site-packages\gtfslib
 
-tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/gtfslib apps/Python27/Lib/site-packages/gtfslib-%V%-py2.7.egg-info
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/gtfslib apps/Python27/Lib/site-packages/gtfslib-1.0.0-py2.7.egg-info
 
 ::--------- Installation
 scp %PKG_BIN% %R%

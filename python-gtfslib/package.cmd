958d89a684332017a7705c4dc07069dcdacc88dd
Hugo Mercier <hugo.mercier@oslandia.com>
2017-05-19 15:12:44 +0200

    Fix gtfslib patch

--- python-gtfslib/package.cmd
+++ python-gtfslib/package.cmd
@@ -18,10 +18,7 @@ call %OSGEO4W_ROOT%\etc\ini\python-core.bat
 
 pip install gtfslib
 
-mkdir gtfslib
-copy %OSGEO4W_ROOT%\apps\python27\lib\site-packages\gtfslib\converter.py gtfslib
-patch -p1 < do_not_prefetch.patch 
-copy /Y gtfslib\converter.py %OSGEO4W_ROOT%\apps\python27\lib\site-packages\gtfslib
+copy /Y converter.py %OSGEO4W_ROOT%\apps\python27\lib\site-packages\gtfslib\converter.py
 
 tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/gtfslib apps/Python27/Lib/site-packages/gtfslib-1.0.0-py2.7.egg-info
 

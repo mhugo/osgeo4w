b76f6f6e323cdc53d4fce2ee24a180b61fa5276a
Hugo Mercier <hugo.mercier@oslandia.com>
2017-05-22 16:37:26 +0200

    Add docopt

---
+++ python-docopt/package.cmd
@@ -0,0 +1,33 @@
+::--------- Package settings --------
+:: package name
+set P=python-docopt
+:: version
+set V=0.6.2
+:: package version
+set B=1
+
+set HERE=%CD%
+
+::--------- Prepare the environment
+call ..\inc\prepare_env.bat %1
+set OSGEO4W_ROOT=c:\osgeo4w64
+set PATH=%OSGEO4W_ROOT%\bin;%PATH%
+
+:: python2 package
+call %OSGEO4W_ROOT%\etc\ini\python-core.bat
+
+pip install docopt==%V% || goto :error
+
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/docopt-%V%.dist-info || goto :error
+
+::--------- Installation
+scp %PKG_BIN% %R% || goto :error
+cd %HERE%
+scp setup.hint %R% || goto :error
+
+goto :EOF
+
+:error
+echo Build failed
+exit /b 1
+

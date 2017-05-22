9889cd036e1bb818166c08717f545406bf78d0c8
Hugo Mercier <hugo.mercier@oslandia.com>
2017-05-22 16:47:09 +0200

    Fix python3-pyqtree

--- python3-pyqtree/package.cmd
+++ python3-pyqtree/package.cmd
@@ -4,7 +4,7 @@ set P=python3-pyqtree
 :: version
 set V=0.24
 :: package version
-set B=2
+set B=3
 
 set HERE=%CD%
 
@@ -16,11 +16,18 @@ set PATH=%OSGEO4W_ROOT%\bin;%PATH%
 :: python2 package
 call %OSGEO4W_ROOT%\bin\py3_env.bat
 
-python3 -m pip install Pyqtree==0.24
+python3 -m pip install Pyqtree==0.24 || goto :error
 
-tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python36/Lib/site-packages/pyqtree.py
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python36/Lib/site-packages/pyqtree.py apps/Python36/Lib/site-packages/Pyqtree-0.24.dist-info || goto :error
 
 ::--------- Installation
-scp %PKG_BIN% %R%
+scp %PKG_BIN% %R% || goto :error
 cd %HERE%
-scp setup.hint %R%
+scp setup.hint %R% || goto :error
+
+goto :EOF
+
+:error
+echo Build failed
+exit /b 1
+

595b000a345f3fa18dca787dd4d06951511ca7bf
vmora <vincent.mora@oslandia.com>
2017-08-11 17:48:01 +0200

    restore failed attempt for empty package

--- freewat/package.cmd
+++ freewat/package.cmd
@@ -12,9 +12,9 @@ set HERE=%CD%
 call ..\inc\prepare_env.bat %1
 
 :: create an empty archive since this is a meta package (dependencies only)
-::copy empty.tar.bz2 %PKG_BIN%
+copy empty.tar.bz2 %PKG_BIN%
 
 ::--------- Installation
-::scp %PKG_BIN% %R%
+scp %PKG_BIN% %R%
 cd %HERE%
 scp setup.hint %R%

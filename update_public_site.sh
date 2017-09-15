4e651bfb40e248279f36be3e0fd07a7be45636d0
Vincent Mora <vincent.mora@oslandia.com>
2017-09-15 13:56:46 +0200

    update script now create a real mirror

--- update_public_site.sh
+++ update_public_site.sh
@@ -3,18 +3,7 @@
 #
 # ssh access is required for server (ftp.cluster023.hosting.ovh.net)
 
-# extract list of packages from official setup.ini
-# extract list of packages from custom setup.ini
-# 
-# for source in [official, custom]:
-#   for package in source
-#     if package_is_availabe_in_public and checksum_matches:
-#         pass
-#     else:
-#         dowload
-#    
-# Note: the custom redirects to official and does the overloading, so we actually
-#       don't look into official directory (only one element in the outer loop)
+# mirrors the official osgeo4w to osgeo4w-oslandia.com
 
 # utilit function to filter wget output and display progress
 progressfilt ()
@@ -50,42 +39,94 @@ progressfilt ()
 official="download.osgeo.org/osgeo4w" 
 custom="osgeo4w.oslandia.net/osgeo4w"
 server="ftp.cluster023.hosting.ovh.net"
-public="osgeo4w-oslandia.com/osgeo4w"
+public="osgeo4w-oslandia.com"
+mirror="mirror"
+extra="extra"
 
-for source in $custom; do
-    echo transfering setup.ini.bz2 from $source
-    { wget --progress=bar:force -O- $source/x86_64/setup.ini.bz2 2>&3 | ssh $server "cat > www/osgeo4w/x86_64/setup.ini.bz2" ; } 3>&1 1>&2 | progressfilt x86_64/setup.ini.bz2 > /dev/stdout
-    printf "\runzip $server/wwww/osgeo4w/x86_64/setup.ini.bz2 and get it's contend\n"
-    ssh $server "bzip2 -dfk www/osgeo4w/x86_64/setup.ini.bz2"
+rep=$mirror
+src=$official
 
-    setup=$(wget -q -O- $public/x86_64/setup.ini| grep x86_64/release )
-    packages=$(printf "$setup" | cut -f2 -d' ')
-    dest_files=$(printf "$packages" | sed 's|\(.*\)|www/osgeo4w/\1|' | xargs echo)
+echo ----------- MIRROR -----------
+# MIRROR
+echo transfering setup.ini.bz2 from $src
+ssh $server "rm -f www/$rep/x86_64/setup.ini.bz2"
+ssh $server "mkdir -p www/$rep/x86_64"
+{ wget --progress=bar:force -O- $src/x86_64/setup.ini.bz2 2>&3 | ssh $server "cat > www/$rep/x86_64/setup.ini.bz2" ; } 3>&1 1>&2 | progressfilt x86_64/setup.ini.bz2 > /dev/stdout
+printf "\runzip $server/wwww/$rep/x86_64/setup.ini.bz2 and get it's contend\n"
+ssh $server "bzip2 -dfk www/$rep/x86_64/setup.ini.bz2"
 
-    nb_pack=$(printf "$setup" | wc -l)
+setup=$(wget -q -O- $public/$rep/x86_64/setup.ini| grep x86_64/release )
+packages=$(printf "$setup" | cut -f2 -d' ')
+dest_files=$(printf "$packages" | sed "s|\(.*\)|www/$rep/\1|" | xargs echo)
 
-    dest_dir=$(dirname $dest_files| xargs echo)
+nb_pack=$(printf "$setup" | wc -l)
 
-    echo creating directory structure in $server
-    ssh $server "mkdir -p $dest_dir"
+dest_dir=$(dirname $dest_files| xargs echo)
 
-    #echo $dest_dir
-    echo getting md5 sums from $server
-    dest_md5=$(ssh $server "md5sum $dest_files" 2> /dev/null | sort | uniq)
-    i=0
-    printf "$setup" | sort | uniq | while read package; do
-        arr=($package)
-        md5=${arr[3]}
-        fil=${arr[1]}
-        res=$(printf "$dest_md5" | grep $fil | cut -f1 -d' ')
-        ((i++))
-        progress=$(echo "scale=2; (100.0*$i)/$nb_pack" | bc)
-        if [ "$md5" != "$res" ]; then
-            LC_NUMERIC="C" printf "\r%-$(($(tput cols) - 8))s %5.1f%%\n" "$fil" $progress
-            { wget --progress=bar:force -O- $source/$fil 2>&3 | ssh $server "cat > www/osgeo4w/$fil" ; } 3>&1 1>&2 | progressfilt $fil
+echo creating directory structure in $server
+ssh $server "mkdir -p $dest_dir"
 
-        fi
-    done
-    echo
+#echo $dest_dir
+echo getting md5 sums from $server
+dest_md5=$(ssh $server "md5sum $dest_files" 2> /dev/null | sort | uniq)
+i=0
+printf "$setup" | sort | uniq | while read package; do
+    arr=($package)
+    md5=${arr[3]}
+    fil=${arr[1]}
+    res=$(printf "$dest_md5" | grep $fil | cut -f1 -d' ')
+    ((i++))
+    progress=$(echo "scale=2; (100.0*$i)/$nb_pack" | bc)
+    if [ "$md5" != "$res" ]; then
+        LC_NUMERIC="C" printf "\r%-$(($(tput cols) - 8))s %5.1f%%\n" "$fil" $progress
+        { wget --progress=bar:force -O- $src/$fil 2>&3 | ssh $server "cat > www/$rep/$fil" ; } 3>&1 1>&2 | progressfilt $fil
+    fi
 done
+echo
+
+echo ----------- EXTRA -----------
+rep=$extra
+src=$custom
+
+echo create links from extra
+ssh $server "mkdir -p www/$extra/x86_64/release"
+#ssh $server "find www/$extra/x86_64/release -type l | xargs rm"
+#ssh $server "ln -s $PWD/www/$mirror/x86_64/release/*  www/$extra/x86_64/release"
+ssh $server "cp -rf www/$mirror/x86_64/release/*  www/$extra/x86_64/release/"
+
+echo transfering setup.ini.bz2 from $src
+ssh $server "rm -f www/$rep/x86_64/setup.ini.bz2"
+ssh $server "mkdir -p www/$rep/x86_64"
+{ wget --progress=bar:force -O- $src/x86_64/setup.ini.bz2 2>&3 | ssh $server "cat > www/$rep/x86_64/setup.ini.bz2" ; } 3>&1 1>&2 | progressfilt x86_64/setup.ini.bz2 > /dev/stdout
+printf "\runzip $server/wwww/$rep/x86_64/setup.ini.bz2 and get it's contend\n"
+ssh $server "bzip2 -dfk www/$rep/x86_64/setup.ini.bz2"
+
+setup=$(wget -q -O- $public/$rep/x86_64/setup.ini| grep x86_64/release )
+packages=$(printf "$setup" | cut -f2 -d' ')
+dest_files=$(printf "$packages" | sed "s|\(.*\)|www/$rep/\1|" | xargs echo)
+
+nb_pack=$(printf "$setup" | wc -l)
+
+dest_dir=$(dirname $dest_files| xargs echo)
+
+echo creating directory structure in $server
+ssh $server "mkdir -p $dest_dir"
+
+#echo $dest_dir
+echo getting md5 sums from $server
+dest_md5=$(ssh $server "md5sum $dest_files" 2> /dev/null | sort | uniq)
+i=0
+printf "$setup" | sort | uniq | while read package; do
+    arr=($package)
+    md5=${arr[3]}
+    fil=${arr[1]}
+    res=$(printf "$dest_md5" | grep $fil | cut -f1 -d' ')
+    ((i++))
+    progress=$(echo "scale=2; (100.0*$i)/$nb_pack" | bc)
+    if [ "$md5" != "$res" ]; then
+        LC_NUMERIC="C" printf "\r%-$(($(tput cols) - 8))s %5.1f%%\n" "$fil" $progress
+        { wget --progress=bar:force -O- $src/$fil 2>&3 | ssh $server "cat > www/$rep/$fil" ; } 3>&1 1>&2 | progressfilt $fil
+    fi
+done
+echo
 

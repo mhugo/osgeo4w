a6a99123d7ff727c696903c5d3a976011302d992
Vincent Mora <vincent.mora@oslandia.com>
2017-09-20 16:03:20 +0200

    adds seaborn package

---
+++ python-seaborn/package.cmd
@@ -0,0 +1,26 @@
+::--------- Package settings --------
+:: package name
+set P=python-seaborn
+:: version
+set V=0.8.1
+:: package version
+set B=1
+
+set HERE=%CD%
+
+::--------- Prepare the environment
+call ..\inc\prepare_env.bat %1
+set OSGEO4W_ROOT=c:\osgeo4w64
+set PATH=%OSGEO4W_ROOT%\bin;%PATH%
+
+:: python2 package
+call %OSGEO4W_ROOT%\etc\ini\python-core.bat
+
+pip install seaborn==%V%
+
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/seaborn*
+
+::--------- Installation
+scp %PKG_BIN% %R%
+cd %HERE%
+scp setup.hint %R%

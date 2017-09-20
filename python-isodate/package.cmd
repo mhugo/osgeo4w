0747dfabbed8b98416c75de12c1f38a9a05e5ba5
Vincent Mora <vincent.mora@oslandia.com>
2017-09-20 16:44:19 +0200

    adds isodate package

---
+++ python-isodate/package.cmd
@@ -0,0 +1,26 @@
+::--------- Package settings --------
+:: package name
+set P=python-isodate
+:: version
+set V=0.5.4
+:: package version
+set B=1
+
+set HERE=%CD%
+
+::--------- Prepare the environment
+call ..\inc\prepare_env.bat %1
+set OSGEO4W_ROOT=c:\osgeo4w64
+set PATH=%OSGEO4W_ROOT%\bin;%PATH%
+
+:: python2 package
+call %OSGEO4W_ROOT%\etc\ini\python-core.bat
+
+pip install isodate==%V%
+
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/isodate*
+
+::--------- Installation
+scp %PKG_BIN% %R%
+cd %HERE%
+scp setup.hint %R%

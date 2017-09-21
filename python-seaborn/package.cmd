a73cb4b38d69355d17689033090dfd924993e253
Vincent Mora <vincent.mora@oslandia.com>
2017-09-21 10:42:39 +0200

    freewat fixup

--- python-seaborn/package.cmd
+++ python-seaborn/package.cmd
@@ -18,7 +18,7 @@ call %OSGEO4W_ROOT%\etc\ini\python-core.bat
 
 pip install seaborn==%V%
 
-tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/seaborn*
+tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% apps/Python27/Lib/site-packages/seaborn apps/Python27/Lib/site-packages/seaborn-%V%.dist-info 
 
 ::--------- Installation
 scp %PKG_BIN% %R%

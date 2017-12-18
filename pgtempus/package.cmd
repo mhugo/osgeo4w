f50a771864d1cfc3a62b433f51f4ad5825eb69e1
Hugo Mercier <hugo.mercier@oslandia.com>
2017-12-18 16:42:14 +0100

    Fix pgtempus package

--- pgtempus/package.cmd
+++ pgtempus/package.cmd
@@ -2,9 +2,9 @@
 :: package name
 set P=pgtempus
 :: version
-set V=1.0.1
+set V=1.1.0
 :: package version
-set B=2
+set B=1
 
 set HERE=%CD%
 
@@ -14,7 +14,10 @@ call ..\inc\prepare_env.bat %1
 :: python3 env
 call C:\osgeo4W64\bin\py3_env.bat || goto :error
 
-wget -O pgtempus.tar.bz2 https://gitlab.com/Oslandia/pgtempus/repository/archive.tar.bz2?ref=v%V% || goto :error
+if "%1"=="test" (
+wget -O pgtempus.tar.bz2 https://gitlab.com/Oslandia/pgtempus/repository/master/archive.tar.bz2 || goto :error
+) else (
+wget -O pgtempus.tar.bz2 https://gitlab.com/Oslandia/pgtempus/repository/archive.tar.bz2?ref=v%V% || goto :error)
 tar xjvf pgtempus.tar.bz2 || goto :error
 cd pgtempus-* || goto :error
 

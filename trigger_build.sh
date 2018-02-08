7c85e6765b91d8cbbea153e0bf6bf12e12338b41
Hugo Mercier <hugo.mercier@oslandia.com>
2018-02-08 10:14:32 +0100

    trigger_build: add link to pipeline (fixes #15)

--- trigger_build.sh
+++ trigger_build.sh
@@ -3,10 +3,14 @@ if [ -z "$1" ]; then
     echo "Argument: package_name release|test"
     exit 1
 fi
-curl --request POST \
+out=$(curl --request POST \
      --form token=983ea4f789964e464c97dd1f7028b9 \
      --form ref=master \
      --form "variables[PACKAGE_NAME]=$1" \
      --form "variables[DELIVERY_ENV]=$2" \
-     https://git.oslandia.net/api/v3/projects/140/trigger/builds
+     https://git.oslandia.net/api/v4/projects/140/trigger/pipeline)
+url=$(echo $out| python3 -c "import sys; import json; print('https://git.oslandia.net/Oslandia-infra/osgeo4w/pipelines/{}'.format(json.load(sys.stdin)['id']))")
+echo $out
+echo "Pipeline:" $url
+xdg-open $url
 

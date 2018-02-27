488566d3742a003e6d5767fd95c2cdcade86dccd
Vincent Mora <vincent.mora@oslandia.com>
2018-02-27 11:40:53 +0100

    adds cgal package

---
+++ packages/cgal/package.cmd
@@ -0,0 +1,43 @@
+::--------- Package settings --------
+:: package name
+set P=tempus-core
+:: version
+set V=4.11.1
+:: package version
+set B=1
+
+::--------- Prepare the environment
+call ..\__inc__\prepare_env.bat %1
+
+set HERE=%CD%
+
+rd /s /q c:\install
+mkdir c:\install
+
+c:\osgeo4w64\bin\osgeo4w-setup.exe -s %OSGEO4W_REPO% -k -q -P boost-devel-vc14 || goto :error
+wget --progress=bar:force https://github.com/CGAL/cgal/archive/releases/CGAL-4.11.1.tar.gz  || goto :err
+tar xjf CGAL-4.11.1.tar.gz
+cd cgal-releases-CGAL-4.11.1
+mkdir build
+cd build
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=C:\install .. || goto :error
+nmake || goto :error
+nmake install || goto :error
+
+:: binary archive
+tar -C c:\install -cjvf %PKG_BIN% lib share include || goto :error
+
+:: source archive
+tar -C %HERE% --transform 's,^,osgeo4w/,' -cvjf %PKG_SRC% package.cmd setup.hint || goto :error
+
+::--------- Installation
+scp %PKG_BIN% %PKG_SRC% %R% || goto :error
+cd %HERE%
+scp setup.hint %R% || goto :error
+goto :EOF
+
+:error
+echo Build failed
+exit /b 1
+  
+  

ace91e96d2580faaf050c48684f12f496e90b859
Vincent Mora <vincent.mora@oslandia.com>
2018-03-22 10:26:02 +0100

    move mpfr dll to bin

--- packages/mpfr/package.cmd
+++ packages/mpfr/package.cmd
@@ -4,7 +4,7 @@ set P=mpfr
 :: version
 set V=3.9.0
 :: package version
-set B=2
+set B=3
 
 ::--------- Prepare the environment
 call ..\__inc__\prepare_env.bat %1
@@ -14,9 +14,11 @@ set HERE=%CD%
 wget --progress=bar:force https://cgal.geometryfactory.com/CGAL/precompiled_libs/auxiliary/x64/MPFR/3.0.0/mpfr-all-CGAL-3.9.zip || goto :err
 unzip mpfr-all-CGAL-3.9.zip || goto :error
 
+mkdir bin
+move lib\*.dll bin\
 
 :: binary archive
-tar -cvjf %PKG_BIN% include lib mpfr.* || goto :error
+tar -cvjf %PKG_BIN% include bin lib mpfr.* || goto :error
 
 :: source archive
 tar -C %HERE% --transform 's,^,osgeo4w/,' -cvjf %PKG_SRC% package.cmd setup.hint || goto :error

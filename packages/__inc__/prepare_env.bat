fe3eff097609745a5f3b2eaa083283c4dabfd8ed
Vincent Mora <vincent.mora@oslandia.com>
2018-03-28 14:48:47 +0200

    fixup for albion

--- packages/__inc__/prepare_env.bat
+++ packages/__inc__/prepare_env.bat
@@ -8,7 +8,7 @@
 call c:\osgeo4w64\bin\o4w_env.bat
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 set PATH=%PATH%;C:\Program Files\CMake\bin;C:\cygwin64\bin
-::;C:\Program Files\PostgreSQL\9.6\bin
+
 echo "Deploy = %1"
 if "%1"=="release" (
 set RELEASE_HOST=ci@hekla.oslandia.net
@@ -20,6 +20,14 @@ set RELEASE_HOST=ci@hekla.oslandia.net
 set RELEASE_PATH=/mnt/osgeo4w_ftp/www/extra.test/x86_64/release/extra
 set OSGEO4W_REPO=http://osgeo4w-oslandia.com/extra.test
 )
+::
+::--------- Prepare the environment
+set PKG_BIN=%P%-%V%-%B%.tar.bz2
+set PKG_SRC=%P%-%V%-%B%-src.tar.bz2
+set D=%RELEASE_PATH%/%P%
+set R=%RELEASE_HOST%:%D%
+ssh %RELEASE_HOST% "mkdir -p %D%"
+set HERE=%CD%
 
 ::--- install needed dependencies
 if "%BUILD_DEPS%" == "" goto nodeps
@@ -37,11 +45,4 @@ echo Problem installing dependencies %BUILD_DEPS_P%
 exit /b 1
 :nodeps
 
-::--------- Prepare the environment
-set PKG_BIN=%P%-%V%-%B%.tar.bz2
-set PKG_SRC=%P%-%V%-%B%-src.tar.bz2
-set D=%RELEASE_PATH%/%P%
-set R=%RELEASE_HOST%:%D%
-ssh %RELEASE_HOST% "mkdir -p %D%"
-set HERE=%CD%
 
--- packages/qgis-ltr-albion-plugin/package.cmd
+++ packages/qgis-ltr-albion-plugin/package.cmd
@@ -14,18 +14,19 @@ set BUILD_DEPS=python-core
 call ..\__inc__\prepare_env.bat %1
 set OSGEO4W_ROOT=c:\osgeo4w64
 set PATH=%OSGEO4W_ROOT%\bin;%PATH%
+set PYTHONPATH=%PYTHONPATH%;%HERE%
 
 :: python2 package
 call %OSGEO4W_ROOT%\etc\ini\python-core.bat || goto :error
 
 git clone --depth 1 https://github.com/Oslandia/albion.git || goto :error
-cd albion
-python -m albion.package -i %OSGEO4W_ROOT%\apps\qgis-ltr\python\plugins
+mkdir install
+python -m albion.package -i install
+dir install
+dir install\albion
 
-cd %HERE%
-
-tar -C %OSGEO4W_ROOT% -cvjf %PKG_BIN% ^
-  apps/qgis-ltr/python/plugins/albion || goto :error
+echo --transform 's,install/,apps/qgis-ltr/python/plugins/,' -cvjf %PKG_BIN% install
+tar --transform 's,install/,apps/qgis-ltr/python/plugins/,' -cvjf %PKG_BIN% install || goto :error
 
 ::--------- Installation
 scp %PKG_BIN% %R% || goto :error

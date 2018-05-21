577925820f65133f6f32c802b6bfbc6d99bda8fc
Hugo Mercier <hugo.mercier@oslandia.com>
2018-05-21 10:00:41 +0200

    Don't update setup.ini before the syncing is complete
    Adapted from https://gitlab.com/jef-n/osgeo4w/commit/fcad85d7b82ea46941964f2e084a6fe2a2e76ec4

--- scripts/mirror-osgeo4w.sh
+++ scripts/mirror-osgeo4w.sh
@@ -12,20 +12,32 @@ echo "Starting sync..."
 (
     #exec >>/tmp/osgeo4w-mirror.log 2>&1
 
-    rm -f /tmp/osgeo4w-files
+    rm -f /tmp/osgeo4w-files /tmp/osgeo4w-files2
 
     for a in x86 x86_64; do
 	mkdir -p $a
 	echo "$(date): Downloading $a/setup.ini."
-	rsync $from/$a/setup.ini.bz2 $a/setup.ini.bz2
-	bzcat $a/setup.ini.bz2 >$a/setup.ini
+	rsync $from/$a/setup.ini.bz2 $a/setup.ini.bz2.syncing
+	bzcat $a/setup.ini.bz2 >$a/setup.ini.syncing
 
-	perl -ne 'print "/$1\n" if /^(?:install|source|license): (\S+) .*$/;' $a/setup.ini >>/tmp/osgeo4w-files
+	perl -ne 'print "/$1\n" if /^(?:install|source|license): (\S+) .*$/;' $a/setup.ini.syncing >>/tmp/osgeo4w-files
 	# add setup.hint
 	while IFS='' read -r line; do echo $(dirname $line)/setup.hint; done < /tmp/osgeo4w-files | sort | uniq >/tmp/osgeo4w-files2
 	cat /tmp/osgeo4w-files >> /tmp/osgeo4w-files2
     done
 
-    rsync --delete --inplace -av --stats --files-from=/tmp/osgeo4w-files2 $from/ ./
+    echo "$(date): Syncing files"
+    rsync --inplace -av --stats --files-from=/tmp/osgeo4w-files2 $from/ ./
+    
+    for a in x86 x86_64; do
+        mv $a/setup.ini.bz2.syncing $a/setup.ini.bz2
+        mv $a/setup.ini.syncing $a/setup.ini
+        echo /$a/setup.ini >>/tmp/osgeo4w-files2
+    done    
+
+    echo "$(date): Deleting obsolete files [$?]"
+
+    find . -type f | sed -e "s/^\.//" | fgrep -v -f /tmp/osgeo4w-files2 | sed -e "s/^/\./" | xargs echo rm -v
+    
     echo "$(date): Syncing done [$?]"
 )

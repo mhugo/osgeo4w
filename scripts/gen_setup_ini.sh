44544fa9afacfbc2d5a403598e8b1a12dcbd6214
Hugo Mercier <hugo.mercier@oslandia.com>
2018-06-07 09:34:49 +0200

    Fix ssh_pass calls

--- scripts/gen_setup_ini.sh
+++ scripts/gen_setup_ini.sh
@@ -8,7 +8,7 @@ else
 fi
 ftp="osgeowosid@ftp.cluster023.hosting.ovh.net"
 
-sshpass -f /home/ci/.ovh_ftp_pass scp $DIR/genini $ftp:
+sshpass -f /home/ci/.ovh_ftp_pass scp $DIR/genini $ftp
 sshpass -f /home/ci/.ovh_ftp_pass ssh $ftp "cd www/$target; ../../genini --arch x86_64 --recursive --output=x86_64/setup.ini x86_64"
 sshpass -f /home/ci/.ovh_ftp_pass ssh $ftp "rm -f www/$target/x86_64/setup.ini.bz2; bzip2 -k www/$target/x86_64/setup.ini"
 cd $o

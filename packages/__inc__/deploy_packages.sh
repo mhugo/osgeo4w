18673961ae4fb46703d70aaba0756bc77688082f
Hugo Mercier <hugo.mercier@oslandia.com>
2018-06-07 16:37:58 +0200

    Allow to override packages

--- packages/__inc__/deploy_packages.sh
+++ packages/__inc__/deploy_packages.sh
@@ -1,6 +1,7 @@
 # $1 target (test|release)
 # $2 arch (x86|x86_64)
 # $3 local absolute path to be deployed
+# $4 override main packages ? (override|)
 ARCH=$2
 P=$3
 if [ "$1" = "release" ]; then 
@@ -12,4 +13,8 @@ if [ "$1" = "test" ]; then
     RELEASE_PATH=/mnt/osgeo4w_ftp/www/extra.test/${ARCH}/release/extra
 fi
 
-scp -r ${P}/* ${RELEASE_HOST}:${RELEASE_PATH}
+if [ "$4" = "override" ]; then
+    scp -r ${P}/* ${RELEASE_HOST}:${RELEASE_PATH}/../zzz_override
+else
+    scp -r ${P}/* ${RELEASE_HOST}:${RELEASE_PATH}
+fi

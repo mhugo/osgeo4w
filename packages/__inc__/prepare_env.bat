3bf1eb4d7543a6d5f221ae3e93cd7f2dceb4c559
Hugo Mercier <hugo.mercier@oslandia.com>
2018-07-13 17:29:19 +0200

    Fix prepare_env

--- packages/__inc__/prepare_env.bat
+++ packages/__inc__/prepare_env.bat
@@ -20,14 +20,6 @@ set RELEASE_HOST=ci@hekla.oslandia.net
 set RELEASE_PATH=/mnt/osgeo4w_ftp/www/extra.test/x86_64/release/extra
 set OSGEO4W_REPO=http://osgeo4w-oslandia.com/extra.test
 )
-::
-::--------- Prepare the environment
-set PKG_BIN=%P%-%V%-%B%.tar.bz2
-set PKG_SRC=%P%-%V%-%B%-src.tar.bz2
-set D=%RELEASE_PATH%/%P%
-set R=%RELEASE_HOST%:%D%
-ssh %RELEASE_HOST% "mkdir -p %D%"
-set HERE=%CD%
 
 ::--- install needed dependencies
 if "%BUILD_DEPS%" == "" goto nodeps
@@ -47,5 +39,13 @@ if %errorlevel% == 1 (
 )
 endlocal
 
+::--------- Prepare the environment
+:nodeps
+set PKG_BIN=%P%-%V%-%B%.tar.bz2
+set PKG_SRC=%P%-%V%-%B%-src.tar.bz2
+set D=%RELEASE_PATH%/%P%
+set R=%RELEASE_HOST%:%D%
+ssh %RELEASE_HOST% "mkdir -p %D%"
+set HERE=%CD%
 
 
